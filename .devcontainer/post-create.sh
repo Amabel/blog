1ad8349de2fe81167d2d5ed42b5859fa04f527cc
Amabel <luoweibinb@gmail.com>
2023-05-07 03:34:23 +0000

    chore: update config

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -9,3 +9,6 @@ wget https://raw.githubusercontent.com/Amabel/dotfiles-for-dev-container/master/
 source ~/.zshrc
 
 git clone https://github.com/paulirish/git-open.git $ZSH_CUSTOM/plugins/git-open
+
+git config --global user.name "Amabel"
+git config --global user.email "luoweibinb@gmail.com"
